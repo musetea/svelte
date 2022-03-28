229f8438cf9d5e077473b0bf350616fbc447c1d5
musetea <dev0019@naver.com>
2022-03-28 22:26:55 +0900

    jeda

---
+++ jeda/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Button from "./Room.svelte";
+  let blast;
+
+  const detonateBomb = event => {
+    console.log(event.detail.blast);
+    blast = event.detail.blast;
+  };
+</script>
+
+<h1>WEB JEDA</h1>
+<div class="lab">
+  <div class="bomb" class:blast />
+  <div class="button">
+    <Button on:click={detonateBomb} />
+  </div>
+</div>
+
+<style>
+  .lab {
+    position: relative;
+    display: grid;
+    place-items: center;
+    height: 100%;
+    width: 100%;
+  }
+  .bomb {
+    width: 20px;
+    height: 20px;
+    border-radius: 100%;
+    border: 4px solid;
+    background: gray;
+    transition: transform 2s ease-in-out;
+  }
+
+  .button {
+    position: absolute;
+    bottom: 0;
+    transition: tr;
+  }
+
+  .blast {
+    background: red;
+    border-color: red;
+    transform: scale(30);
+  }
+</style>
---
+++ jeda/src/Room.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const send = createEventDispatcher();
+
+  const goAhead = () => {
+    send("click", {
+      blast: true,
+    });
+  };
+</script>
+
+<div class="safe-room">
+  <button on:click={goAhead}> Detonate </button>
+</div>
+
+<style>
+  .safe-room {
+    width: 200px;
+    height: 100px;
+    border: 4px solid;
+    background: white;
+
+    display: grid;
+    place-items: center;
+  }
+</style>
